060580243eb22e3e59e286b4f55b4529cabed1e5
Stephan Zeissler <stephan@stormforger.com>
2022-12-20 17:34:23 +0100

    2022 / Day 03 / Part 2

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Rucksack {
     pub left: String,
     pub right: String,
@@ -6,20 +6,23 @@ pub struct Rucksack {
 
 mod parser {
     pub fn parse(input: &str) -> Vec<crate::Rucksack> {
-        input.lines().map(|line| {
-            let len = line.len() / 2;
-            crate::Rucksack{
-                left: line.chars().take(len).collect(),
-                right: line.chars().skip(len).collect(),
-            }
-        }).collect()
+        input
+            .lines()
+            .map(|line| {
+                let len = line.len() / 2;
+                crate::Rucksack {
+                    left: line.chars().take(len).collect(),
+                    right: line.chars().skip(len).collect(),
+                }
+            })
+            .collect()
     }
 
     pub fn parse_priority(input: char) -> u32 {
         match input {
-        'a'..='z' => input as u32 - 'a' as u32 + 1,
-        'A'..='Z' => input as u32 - 'A' as u32 + 27,
-        _ => panic!("unsupported input: {}", input)
+            'a'..='z' => input as u32 - 'a' as u32 + 1,
+            'A'..='Z' => input as u32 - 'A' as u32 + 27,
+            _ => panic!("unsupported input: {}", input),
         }
     }
 
@@ -38,22 +41,24 @@ mod parser {
         assert_eq!("ab", r[0].left);
         assert_eq!("bc", r[0].right);
 
-        r = parse("vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL");
+        r = parse(
+            "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        );
         assert_eq!(2, r.len());
         assert_eq!("vJrwpWtwJgWr", r[0].left);
         assert_eq!("hcsFMMfFFhFp", r[0].right);
         assert_eq!("jqHRNqRjqzjGDLGL", r[1].left);
         assert_eq!("rsFMfFZSrLrFZsSL", r[1].right);
-
     }
 }
 
 mod logic {
     pub fn find_duplicates(left: &String, right: &String) -> Vec<char> {
-        let mut v : Vec<char> = left.chars().flat_map(|c| {
-            right.chars().filter(|c2| c == *c2).collect::<Vec<char>>()
-        }).collect();
+        let mut v: Vec<char> = left
+            .chars()
+            .flat_map(|c| right.chars().filter(|c2| c == *c2).collect::<Vec<char>>())
+            .collect();
         v.dedup();
         v
     }
@@ -63,22 +68,79 @@ mod logic {
         let input = "L".to_string();
         assert_eq!(vec!['L'], find_duplicates(&input, &input));
 
-        assert_eq!(vec!['a'], find_duplicates(&"ab".to_string(), &"ca".to_string()));
-        assert_eq!(vec!['a'], find_duplicates(&"aba".to_string(), &"ca".to_string()));
+        assert_eq!(
+            vec!['a'],
+            find_duplicates(&"ab".to_string(), &"ca".to_string())
+        );
+        assert_eq!(
+            vec!['a'],
+            find_duplicates(&"aba".to_string(), &"ca".to_string())
+        );
+    }
+
+    pub fn find_badge(input: &Vec<crate::Rucksack>) -> char {
+        let first = input[0].clone();
+        // println!("---");
+        // try every char from first on ALL Rucksacks and check if each one has exactly one of these items in either compartment
+        (first.left.clone() + &first.right)
+            .chars()
+            .find(|c| {
+                // println!("{}", c);
+                input.iter().all(|rucksack| {
+                    let count = (rucksack.left.clone() + &rucksack.right)
+                        .chars()
+                        .filter(|rc| rc == c)
+                        .count();
+                    // println!("{}{}  '{}' => {}", rucksack.left, rucksack.right, c, count);
+                    count >= 1
+                })
+            })
+            .unwrap()
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg";
+        let data = crate::parser::parse(input);
+
+        assert_eq!('r', find_badge(&data));
+    }
+
+    #[test]
+    fn test_part2_example2() {
+        let input = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        let data = crate::parser::parse(input);
+
+        assert_eq!('Z', find_badge(&data));
     }
 }
 
 fn part1(input: &Vec<Rucksack>) -> u32 {
-    input.iter().map(|rucksack| {
-        logic::find_duplicates(&rucksack.left, &rucksack.right).iter()
-            // .inspect(|c| println!("{}", c))
-            .map(|c| parser::parse_priority(*c))
-            .sum::<u32>()
-    }).sum()
+    input
+        .iter()
+        .map(|rucksack| {
+            logic::find_duplicates(&rucksack.left, &rucksack.right)
+                .iter()
+                // .inspect(|c| println!("{}", c))
+                .map(|c| parser::parse_priority(*c))
+                .sum::<u32>()
+        })
+        .sum()
 }
 
 fn part2(input: &Vec<Rucksack>) -> u32 {
-    0
+    input
+        .chunks_exact(3)
+        .map(|group| {
+            let tmp = vec![group[0].clone(), group[1].clone(), group[2].clone()];
+            logic::find_badge(&tmp)
+        })
+        .map(|c| parser::parse_priority(c))
+        .sum()
 }
 
 #[test]
@@ -92,6 +154,7 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
 
     let data = parser::parse(input);
     assert_eq!(157, part1(&data));
+    assert_eq!(70, part2(&data));
 }
 
 fn main() {
