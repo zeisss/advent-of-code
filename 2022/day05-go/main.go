e3982218bd873bd7e53328e575355ac5ca8d9562
ZeissS <zeisss@moinz.de>
2023-11-02 15:35:20 +0100

    Day 05 Part 1

---
+++ 2022/day05-go/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	plan, err := ParseFile("./testdata/input.txt")
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+	for plan.HasNextAction() {
+		processNextAction(&plan)
+	}
+	log.Println(GetTopContainerNames(plan.Stacks))
+}
+
+type Plan struct {
+	Stacks  [][]string
+	Actions []Action
+}
+type Action struct {
+	Src, Dst int
+	Amount   int
+}
+
+func (p Plan) HasNextAction() bool {
+	return len(p.Actions) > 0
+}
+
+func ParseFile(filename string) (Plan, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return Plan{}, err
+	}
+	return ParseData(data)
+}
+
+func ParseData(data []byte) (Plan, error) {
+	var plan Plan
+	scanner := bufio.NewScanner(bytes.NewReader(data))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, " 1   2") {
+			continue
+		}
+
+		sliceCount := (len(line) + 1) / 4
+		// log.Printf("stacks: %d", sliceCount)
+
+		// grow stacks as needed
+		for i := len(plan.Stacks); i < sliceCount; i++ {
+			plan.Stacks = append(plan.Stacks, make([]string, 0, 10))
+		}
+
+		// parse line and preprend to stack
+		for i := 0; i < sliceCount; i++ {
+			r := line[i*4+1 : i*4+2]
+			if r != " " {
+				// log.Printf("Stack: %d Container: %v", i, r)
+				plan.Stacks[i] = append([]string{r}, plan.Stacks[i]...)
+			}
+		}
+
+		// log.Println("line: ", line)
+		if line == "" {
+			break // End of stack drawing
+		}
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text() // format: "move X from S to D"
+		// log.Println("line: ", line)
+
+		var action Action
+		_, err := fmt.Sscanf(line, "move %d from %d to %d", &action.Amount, &action.Src, &action.Dst)
+		if err != nil {
+			return Plan{}, err
+		}
+		plan.Actions = append(plan.Actions, action)
+	}
+	return plan, nil
+}
+
+func processNextAction(p *Plan) {
+	if !p.HasNextAction() {
+		panic("no actions left")
+	}
+
+	action := p.Actions[0]
+	p.Actions = p.Actions[1:]
+	p.Stacks = applyAction(p.Stacks, action)
+}
+
+func applyAction(stacks [][]string, action Action) [][]string {
+	src := action.Src - 1
+	dst := action.Dst - 1
+
+	if src > len(stacks) || dst > len(stacks) {
+		panic("invalid action src / dst")
+	}
+	if action.Amount > len(stacks[src]) {
+		panic("not enough containers on src stack")
+	}
+
+	srcLen := len(stacks[src])
+
+	data := stacks[src][srcLen-action.Amount:]
+	stacks[src] = stacks[src][0 : srcLen-action.Amount]
+
+	for i := len(data); i > 0; i-- {
+		stacks[dst] = append(stacks[dst], data[i-1])
+	}
+
+	return stacks
+}
+
+func GetTopContainerNames(stacks [][]string) string {
+	out := ""
+
+	for i := 0; i < len(stacks); i++ {
+		out += stacks[i][len(stacks[i])-1]
+	}
+
+	return out
+}
---
+++ 2022/day05-go/main_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
+	"github.com/stretchr/testify/require"
+)
+
+func TestApplyAction(t *testing.T) {
+	for i := 1; i <= 3; i++ {
+		t.Run(fmt.Sprintf("amount=%d", i), func(t *testing.T) {
+			out := applyAction(
+				[][]string{
+					{"a", "b", "c"},
+					{},
+				},
+				Action{Src: 1, Dst: 2, Amount: i},
+			)
+
+			require.Len(t, out[0], 3-i)
+			require.Len(t, out[1], i)
+		})
+	}
+}
+
+func TestGetTopContainerNames(t *testing.T) {
+	out := [][]string{
+		{"a", "b", "c"},
+		{"d"},
+	}
+	s := GetTopContainerNames(out)
+	require.Equal(t, "cd", s)
+}
+
+func TestParseData(t *testing.T) {
+	input := `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
+	plan, err := ParseData([]byte(input))
+	require.NoError(t, err)
+
+	approvals.VerifyJSONStruct(t, plan)
+}
+
+func TestParseData_Actions(t *testing.T) {
+	for actions := 1; actions <= 4; actions++ {
+		t.Run(fmt.Sprintf("processNextAction() %dx", actions), func(t *testing.T) {
+			input := `    [D]    
+[N] [C]    
+[Z] [M] [P]
+1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
+			plan, err := ParseData([]byte(input))
+			require.NoError(t, err)
+
+			for i := 0; i < actions; i++ {
+				processNextAction(&plan)
+			}
+			approvals.VerifyJSONStruct(t, plan)
+
+		})
+	}
+}
