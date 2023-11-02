db4a950ecd507faf55d627f372477230f1be51f2
ZeissS <zeisss@moinz.de>
2023-11-02 20:52:02 +0100

    Day 08 Part 2 (Go)

--- 2022/day08-go/main.go
+++ 2022/day08-go/main.go
@@ -16,10 +16,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("ERROR(parse): %v", err)
 	}
-	log.Printf("Number of visible trees: %d", CountVisibleTrees(g))
+	log.Printf("Number of visible trees: %d", CountTotalVisibleTreesInGrid(g))
+	log.Printf("Highest Scenic Score: %d", FindHeighestScenicScore(g))
 }
-func Parse(input string) (*Grid, error) {
 
+func Parse(input string) (*Grid, error) {
 	trees := make([][]int, 0, 10)
 
 	lines := strings.Split(input, "\n")
@@ -56,7 +57,7 @@ func (g Grid) HeightAt(x, y int) int {
 	return g.Trees[y][x]
 }
 
-func CountVisibleTrees(g *Grid) int {
+func CountTotalVisibleTreesInGrid(g *Grid) int {
 	visible := g.Width + g.Width + g.Height - 2 + g.Height - 2 // the border is always visible
 
 	for y := 1; y < g.Height-1; y++ {
@@ -69,22 +70,47 @@ func CountVisibleTrees(g *Grid) int {
 	return visible
 }
 
-func TreeIsInvisible(g *Grid, x, y int) bool {
-	targetHeight := g.HeightAt(x, y)
-
-	anyTreeHeigherOrEqual := func(xMod, yMod int) bool {
-		x := x + xMod
-		y := y + yMod
-		for x >= 0 && x < g.Width && y >= 0 && y < g.Height {
-			if g.HeightAt(x, y) >= targetHeight {
-				return true
+func FindHeighestScenicScore(g *Grid) int {
+	score := 0
+	for x := 0; x < g.Width; x++ {
+		for y := 0; y < g.Height; y++ {
+			if s := ScenicScore(g, x, y); s > score {
+				score = s
+			} else if score == s && s > 0 {
+				log.Printf("equal scenic score: %d, %d => %d", x, y, s)
 			}
-			x += xMod
-			y += yMod
 		}
-		return false
 	}
+	return score
+}
 
-	return anyTreeHeigherOrEqual(-1, 0) && anyTreeHeigherOrEqual(1, 0) &&
-		anyTreeHeigherOrEqual(0, -1) && anyTreeHeigherOrEqual(0, +1)
+func TreeIsInvisible(g *Grid, x, y int) bool {
+	return countTreesUntilEqualOrHigher(g, x, y, -1, 0) == x &&
+		countTreesUntilEqualOrHigher(g, x, y, 1, 0)-x == 0 &&
+		countTreesUntilEqualOrHigher(g, x, y, 0, -1) == y &&
+		countTreesUntilEqualOrHigher(g, x, y, 0, +1)-y == 0
+}
+
+func ScenicScore(g *Grid, x, y int) int {
+	return countTreesUntilEqualOrHigher(g, x, y, -1, 0) *
+		countTreesUntilEqualOrHigher(g, x, y, 1, 0) *
+		countTreesUntilEqualOrHigher(g, x, y, 0, -1) *
+		countTreesUntilEqualOrHigher(g, x, y, 0, +1)
+}
+
+func countTreesUntilEqualOrHigher(g *Grid, srcX, srcY, xMod, yMod int) int {
+	targetHeight := g.HeightAt(srcX, srcY)
+
+	x := srcX + xMod
+	y := srcY + yMod
+	trees := 0
+	for x >= 0 && x < g.Width && y >= 0 && y < g.Height {
+		trees++
+		if g.HeightAt(x, y) >= targetHeight {
+			break
+		}
+		x += xMod
+		y += yMod
+	}
+	return trees
 }
--- 2022/day08-go/main_test.go
+++ 2022/day08-go/main_test.go
@@ -30,5 +30,42 @@ func TestCountVisibleTrees(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, data)
 
-	assert.EqualValues(t, 21, CountVisibleTrees(data))
+	assert.EqualValues(t, 21, CountTotalVisibleTreesInGrid(data))
+}
+
+func TestCountSmallerTreesUntilEqualOrHigher(t *testing.T) {
+	data, err := Parse(TEST_INPUT)
+	require.NoError(t, err)
+	require.NotNil(t, data)
+
+	x := 2
+	y := 1
+	assert.EqualValues(t, 1, countTreesUntilEqualOrHigher(data, x, y, 0, -1))
+	assert.EqualValues(t, 1, countTreesUntilEqualOrHigher(data, x, y, -1, 0))
+	assert.EqualValues(t, 2, countTreesUntilEqualOrHigher(data, x, y, +1, 0))
+	assert.EqualValues(t, 2, countTreesUntilEqualOrHigher(data, x, y, 0, +1))
+
+	x = 2
+	y = 3
+	assert.EqualValues(t, 2, countTreesUntilEqualOrHigher(data, x, y, 0, -1))
+	assert.EqualValues(t, 2, countTreesUntilEqualOrHigher(data, x, y, -1, 0))
+	assert.EqualValues(t, 1, countTreesUntilEqualOrHigher(data, x, y, 0, +1))
+	assert.EqualValues(t, 2, countTreesUntilEqualOrHigher(data, x, y, +1, 0))
+}
+
+func TestScenicScore(t *testing.T) {
+	data, err := Parse(TEST_INPUT)
+	require.NoError(t, err)
+	require.NotNil(t, data)
+
+	assert.EqualValues(t, 4, ScenicScore(data, 2, 1))
+	assert.EqualValues(t, 8, ScenicScore(data, 2, 3))
+}
+
+func TestFindHighestScenicScore(t *testing.T) {
+	data, err := Parse(TEST_INPUT)
+	require.NoError(t, err)
+	require.NotNil(t, data)
+
+	assert.EqualValues(t, 8, FindHeighestScenicScore(data))
 }
