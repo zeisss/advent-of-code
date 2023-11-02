c4ea63952fc0706bac4133fff257957ca2066a15
ZeissS <zeisss@moinz.de>
2023-11-02 16:27:51 +0100

    Day 06 Part 2

--- 2022/day06-go/main.go
+++ 2022/day06-go/main.go
@@ -6,33 +6,34 @@ import (
 )
 
 func main() {
-	s, err := ParseFile("testdata/input.txt")
+	s, err := os.ReadFile("./testdata/input.txt")
 	if err != nil {
 		log.Fatalf("ERROR: %v", err)
 	}
-	log.Println(FindMarker(s))
+	log.Println(FindUniqueMarkerPosition(string(s), 14))
 }
 
-func ParseFile(s string) (string, error) {
-	data, err := os.ReadFile(s)
-	if err != nil {
-		return "", err
-	}
-	return string(data), nil
-}
-
-func FindMarker(input string) int {
-	for i := 0; i <= len(input)-4; i++ {
-		if input[i] == input[i+1] ||
-			input[i] == input[i+2] ||
-			input[i] == input[i+3] ||
-			input[i+1] == input[i+2] ||
-			input[i+1] == input[i+3] ||
-			input[i+2] == input[i+3] {
+func FindUniqueMarkerPosition(input string, length int) int {
+	for i := 0; i <= len(input)-length; i++ {
+		if HasDuplicateCharacters(input[i : i+length]) {
 			continue
 		}
-		return i + 4
-	}
 
+		// we processed the loop without a continue, everything should be good!
+		return i + length
+	}
 	return -1
+}
+
+func HasDuplicateCharacters(s string) bool {
+	bytes := [256]byte{}
+
+	for _, b := range []byte(s) {
+		if bytes[b] > 0 {
+			// we got a duplicate, lets start over with the next start position
+			return true
+		}
+		bytes[b]++
+	}
+	return false
 }
--- 2022/day06-go/main_test.go
+++ 2022/day06-go/main_test.go
@@ -6,17 +6,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFindMarker(t *testing.T) {
-	assert.Equal(t, 4, FindMarker("abcd"))
-	assert.Equal(t, 4, FindMarker("abcde"))
-	assert.Equal(t, 5, FindMarker("aabcd"))
-	assert.Equal(t, 5, FindMarker("abcad"))
-	assert.Equal(t, 7, FindMarker("abccdef"))
+func TestFindUniqueMarkerPosition_Length4(t *testing.T) {
+	assert.Equal(t, 4, FindUniqueMarkerPosition("abcd", 4))
+	assert.Equal(t, 4, FindUniqueMarkerPosition("abcde", 4))
+	assert.Equal(t, 5, FindUniqueMarkerPosition("aabcd", 4))
+	assert.Equal(t, 5, FindUniqueMarkerPosition("abcad", 4))
+	assert.Equal(t, 7, FindUniqueMarkerPosition("abccdef", 4))
 
 	// Examples
-	assert.Equal(t, 7, FindMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
-	assert.Equal(t, 5, FindMarker("bvwbjplbgvbhsrlpgdmjqwftvncz"))
-	assert.Equal(t, 6, FindMarker("nppdvjthqldpwncqszvftbrmjlhg"))
-	assert.Equal(t, 10, FindMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
-	assert.Equal(t, 11, FindMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+	assert.Equal(t, 7, FindUniqueMarkerPosition("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4))
+	assert.Equal(t, 5, FindUniqueMarkerPosition("bvwbjplbgvbhsrlpgdmjqwftvncz", 4))
+	assert.Equal(t, 6, FindUniqueMarkerPosition("nppdvjthqldpwncqszvftbrmjlhg", 4))
+	assert.Equal(t, 10, FindUniqueMarkerPosition("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4))
+	assert.Equal(t, 11, FindUniqueMarkerPosition("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4))
+}
+
+func TestFindUniqueMarkerPosition_Length14(t *testing.T) {
+	// Examples
+	assert.Equal(t, 19, FindUniqueMarkerPosition("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14))
+	assert.Equal(t, 23, FindUniqueMarkerPosition("bvwbjplbgvbhsrlpgdmjqwftvncz", 14))
+	assert.Equal(t, 23, FindUniqueMarkerPosition("nppdvjthqldpwncqszvftbrmjlhg", 14))
+	assert.Equal(t, 29, FindUniqueMarkerPosition("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14))
+	assert.Equal(t, 26, FindUniqueMarkerPosition("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14))
+}
+
+func TestHasDuplicateCharacters(t *testing.T) {
+	assert.False(t, HasDuplicateCharacters("ab"), "ab should be false")
+	assert.False(t, HasDuplicateCharacters("abcd"), "abcd should be false")
+	assert.True(t, HasDuplicateCharacters("aa"), "aa should be true")
+	assert.True(t, HasDuplicateCharacters("aba"), "aba should be true")
 }
