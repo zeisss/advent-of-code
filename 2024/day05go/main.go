3715372140cd4c433f2891ce67bf67dd615fa946
ZeissS <zeisss@moinz.de>
2025-02-05 22:39:35 +0100

    2024 Day 05 Part 2

--- 2024/day05go/main.go
+++ 2024/day05go/main.go
@@ -34,8 +34,10 @@ func main() {
 
 	rules, updates := Parse(string(d))
 	p1 := Part1(rules, updates)
+	p2 := Part2(rules, updates)
 
 	log.Printf("Part 1: %d", p1)
+	log.Printf("Part 2: %d", p2)
 }
 
 func Part1(rules Rules, updates Updates) int64 {
@@ -43,6 +45,16 @@ func Part1(rules Rules, updates Updates) int64 {
 	for _, update := range updates {
 		if IsCorrectlyOrdered(rules, update) {
 			output += MiddlePageNumber(update)
+		}
+	}
+	return output
+}
+
+func Part2(rules Rules, updates Updates) int64 {
+	var output int64
+	for _, update := range updates {
+		if !IsCorrectlyOrdered(rules, update) {
+			output += MiddlePageNumber(FixUpdate(rules, update))
 		}
 	}
 	return output
@@ -74,9 +86,24 @@ func IsCorrectlyOrdered(rules Rules, update Update) bool {
 	return true
 }
 
+func FixUpdate(rules Rules, update Update) Update {
+	var fixedUpdate Update = update
+
+	slices.SortFunc(fixedUpdate, func(a, b int64) int {
+		nextPages := rules[a]
+		if slices.Contains(nextPages, b) {
+			return -1
+		} else {
+			return 0
+		}
+	})
+
+	return fixedUpdate
+}
+
 func Parse(input string) (Rules, Updates) {
-	ss := strings.Split(input, "\n\n")
-	return ParseRules(ss[0]), ParseUpdates(ss[1])
+	parts := strings.Split(input, "\n\n")
+	return ParseRules(parts[0]), ParseUpdates(parts[1])
 }
 
 func ParseRules(input string) Rules {
