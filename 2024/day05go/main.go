82cb17a1e5db632bf8037bb83ee8fa8b0cb4c3c4
ZeissS <zeisss@moinz.de>
2025-02-05 22:24:14 +0100

    2024 Day 05 Part 1

---
+++ 2024/day05go/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+/*
+--- Part Two ---
+While the Elves get to work printing the correctly-ordered updates, you have a little time to fix the rest of them.
+
+For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in the right order. For the above example, here are the three incorrectly-ordered updates and their correct orderings:
+
+75,97,47,61,53 becomes 97,75,47,61,53.
+61,13,29 becomes 61,29,13.
+97,13,75,29,47 becomes 97,75,47,29,13.
+After taking only the incorrectly-ordered updates and ordering them correctly, their middle page numbers are 47, 29, and 47. Adding these together produces 123.
+
+Find the updates which are not in the correct order. What do you get if you add up the middle page numbers after correctly ordering just those updates?
+*/
+
+type Rules = map[int64][]int64
+type Update = []int64
+type Updates = []Update
+
+func main() {
+	d, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+
+	rules, updates := Parse(string(d))
+	p1 := Part1(rules, updates)
+
+	log.Printf("Part 1: %d", p1)
+}
+
+func Part1(rules Rules, updates Updates) int64 {
+	var output int64
+	for _, update := range updates {
+		if IsCorrectlyOrdered(rules, update) {
+			output += MiddlePageNumber(update)
+		}
+	}
+	return output
+}
+
+func MiddlePageNumber(update Update) int64 {
+	return update[len(update)/2]
+}
+
+func IsCorrectlyOrdered(rules Rules, update Update) bool {
+	for index, pageNumber := range update {
+		mayNotBeBeforeN, ok := rules[pageNumber]
+		if !ok {
+			// valid
+			continue
+		}
+
+		onlyLeftNumbers := update[0:index]
+
+		// check each rule individually
+		for _, mustBeAfter := range mayNotBeBeforeN {
+			if slices.Contains(onlyLeftNumbers, mustBeAfter) {
+				return false
+			}
+		}
+
+	}
+
+	return true
+}
+
+func Parse(input string) (Rules, Updates) {
+	ss := strings.Split(input, "\n\n")
+	return ParseRules(ss[0]), ParseUpdates(ss[1])
+}
+
+func ParseRules(input string) Rules {
+	rules := Rules{}
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		ss := strings.Split(line, "|")
+
+		i, err := strconv.ParseInt(ss[0], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		j, err := strconv.ParseInt(ss[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		if _, ok := rules[i]; !ok {
+			rules[i] = []int64{j}
+		} else {
+			rules[i] = append(rules[i], j)
+		}
+	}
+
+	return rules
+}
+
+func ParseUpdates(input string) Updates {
+	updates := Updates{}
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		ss := strings.Split(line, ",")
+		var update Update
+
+		for _, page := range ss {
+			n, err := strconv.ParseInt(page, 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			update = append(update, n)
+		}
+
+		updates = append(updates, update)
+	}
+	return updates
+}
---
+++ 2024/day05go/main_test.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var input = `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+
+func TestParseRules(t *testing.T) {
+	rules := ParseRules(`47|53
+97|13
+97|61`)
+	assert.Equal(t, Rules{
+		47: []int64{53},
+		97: []int64{13, 61},
+	}, rules)
+}
+func TestParseUpdates(t *testing.T) {
+	updates := ParseUpdates(`61,13,29
+97,13,75,29,47`)
+	assert.Equal(t, Updates{
+		[]int64{61, 13, 29},
+		[]int64{97, 13, 75, 29, 47},
+	}, updates)
+}
+
+func TestMiddleElement(t *testing.T) {
+	assert.EqualValues(t, 3, MiddlePageNumber([]int64{1, 2, 3, 4, 5}))
+}
+
+func TestIsCorrectlyOrdered(t *testing.T) {
+	// empty rules allow everything
+	assert.True(t, IsCorrectlyOrdered(Rules{}, []int64{1, 2, 3}))
+	assert.True(t, IsCorrectlyOrdered(Rules{
+		1: []int64{5},
+	}, []int64{1, 2, 3, 5}))
+
+	var rules Rules = ParseRules(`47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13`)
+	assert.True(t, IsCorrectlyOrdered(rules, []int64{75, 47, 61, 53, 29}))
+	assert.True(t, IsCorrectlyOrdered(rules, []int64{97, 61, 53, 29, 13}))
+	assert.True(t, IsCorrectlyOrdered(rules, []int64{75, 29, 13}))
+
+	// Negative examples from task
+	assert.False(t, IsCorrectlyOrdered(rules, []int64{75, 97, 47, 61, 53}))
+	assert.False(t, IsCorrectlyOrdered(rules, []int64{61, 13, 29}))
+	assert.False(t, IsCorrectlyOrdered(rules, []int64{97, 13, 75, 29, 47}))
+}
